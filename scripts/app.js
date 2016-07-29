cb74b3d77e55f22203c022e4f3fe2549e6ef1a01
ihptru <ihptru@gmail.com>
2016-07-29 16:11:52 +0300

    order milk form

--- scripts/app.js
+++ scripts/app.js
@@ -4,3 +4,29 @@ $(function () {
   console.log('JS TEST MESSAGE!');
 });
 //# sourceMappingURL=app.js.map
+
+function orderForm()
+{
+	$.fancybox('/ajax.php?order',{
+		type:'ajax',
+		autoSize: true,
+		fitToView: false,
+		maxWidth: "100%",
+		helpers:{
+			overlay:{
+				locked:true
+			}
+		}
+	});
+	return false;
+}
+
+function ajaxFormTry(ajaxForm){
+	$.post(
+		ajaxForm.attr('action'),
+		ajaxForm.serialize(),
+		function(data){
+			$.fancybox.open(data);
+	});
+	return false;
+}
